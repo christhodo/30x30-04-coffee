fc5821cf1b1e03d2966226b5bbc03503d4493d57
christhodo <102981895+christhodo@users.noreply.github.com>
2022-05-20 10:49:49 -0700

    clean-up

--- src/app/common/models/services/drinks.service.ts
+++ src/app/common/models/services/drinks.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const BASE_URL = 'http://localhost:3000/';
+const BASE_URL = 'http://localhost:3000';
 
 @Injectable({
   providedIn: 'root',
@@ -32,10 +32,10 @@ export class DrinksService {
   }
 
   private getUrl() {
-    return `${BASE_URL}${this.model}`;
+    return `${BASE_URL}/${this.model}`;
   }
 
   private getUrlWithID(id) {
-    return `${this.getUrl()}${id}`;
+    return `${this.getUrl()}/${id}`;
   }
 }
